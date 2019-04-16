1a5013645d022d2d80eb72e4b85cba1c2b0d3068
Daniel <hhuwitt@msn.com>
2019-04-16 20:02:57 +0200

    implementing renderArea
    implementing renderArea

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+//#include <QColor>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,28 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_btnAstroid_clicked()
+{
+    this->ui->renderArea->setShape(RenderArea::Astroid);
+    this->ui->renderArea->repaint();
+}
+
+void MainWindow::on_btnCycloid_clicked()
+{
+    this->ui->renderArea->setShape(RenderArea::Cycloid);
+    this->ui->renderArea->repaint();
+}
+
+void MainWindow::on_btnHuygensCycloid_clicked()
+{
+    this->ui->renderArea->setShape(RenderArea::HuygensCycloid);
+    this->ui->renderArea->repaint();
+}
+
+void MainWindow::on_btnHypoCycloid_clicked()
+{
+    this->ui->renderArea->setShape(RenderArea::HypoCycloid);
+    this->ui->renderArea->repaint();
 }
